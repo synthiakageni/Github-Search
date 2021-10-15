99e0a08bb6318d41da59d3994539acae1eb1c30f
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-15 11:31:02 +0300

    import class userService and Users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Users} from '../users';
+import{UserService} from '../user-service';
+import{Repository} from '../repository';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+    
+  users:any=Users;
+  repository:any=Repository;
+
 
   constructor() { }
 
