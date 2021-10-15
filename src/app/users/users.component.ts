bd6d557cb6a0b5fa42e224b40ae0ada8b22966fd
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-15 15:49:36 +0300

    add function search()

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{Users} from '../users';
-import{UserService} from '../user-service';
+import {  UserService } from "../user-service";
 import{Repository} from '../repository';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -11,11 +12,19 @@ export class UsersComponent implements OnInit {
     
   users:any=Users;
   repository:any=Repository;
+  
 
+  constructor(private userService:  UserService, public repos: UserService) { }
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+    Search(username: string){
+      this.userService.getUsers(username);
+      this.repos.getRepo(username);
+      this.users = this.userService.users;
+     this.ngOnInit();
+       }
+
   }
 
-}
+
